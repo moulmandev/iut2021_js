b68c5f0b314f294d0b464ec1c9f338ae9b6f8372
Antoine <antoine_pannecoucke@orange.fr>
2021-03-22 11:52:44 +0100

    Add Vuetify

---
+++ front/vue/src/components/Menu.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div v-for="(item, index) in items" :key="index">
+        <v-card
+            class="ma-4"
+        >
+            {{ item.titre }}
+            {{ item.num }}
+        </v-card>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [{
+          titre: "test",
+          num: 10
+      }, {
+          titre: "vudcqsgsdfuyogs",
+          num: 2054
+      }],
+    };
+  },
+};
+</script>
---
+++ front/vue/src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app-bar app color="#131921" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Amazon logo"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        class="mx-2"
+        fab
+        color="#131921"
+        >
+        <v-icon dark>
+            fa-shopping-cart
+        </v-icon>
+      </v-btn>
+    </v-app-bar>
+</template>
